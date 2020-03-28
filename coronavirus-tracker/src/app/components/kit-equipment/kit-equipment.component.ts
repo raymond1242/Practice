ecc9f3f73a61d51bba8289dfc091de663fa968be
Renato <renato.chavez@ucsp.edu.pe>
2020-03-28 10:59:59 -0500

    improving landing adding counter COVID

--- coronavirus-tracker/src/app/components/kit-equipment/kit-equipment.component.ts
+++ coronavirus-tracker/src/app/components/kit-equipment/kit-equipment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-kit-equipment',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KitEquipmentComponent implements OnInit {
 
-  constructor() { }
+  dataPeru: any = null;
+  dataWorld: any = null;
+  dataHistory: any = null;
+
+  constructor(
+    protected http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.getCOVIDdataPeru().subscribe(
+      response => {
+        this.dataPeru = response[0];
+    });
+    this.getCOVIDdataWorld().subscribe(
+      response => {
+        this.dataWorld = response[0];
+    });
   }
 
+  getCOVIDdataPeru(): Observable<any> {
+    return this.http.get("https://covid-19-data.p.rapidapi.com/country/code?format=undefined&code=pe", {
+      headers: {
+        "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		    "x-rapidapi-key": "b0ec4b8f9amsh6abc78efb1131d0p17b16bjsncff41d672d8f"
+      }
+    });
+  }
+
+  getCOVIDdataWorld(): Observable<any> {
+    return this.http.get("https://covid-19-data.p.rapidapi.com/totals", {
+      headers: {
+        "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+	      "x-rapidapi-key": "b0ec4b8f9amsh6abc78efb1131d0p17b16bjsncff41d672d8f"
+      }
+    });
+  }
 }
